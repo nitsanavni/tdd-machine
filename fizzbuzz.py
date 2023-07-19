b4f0a0519e5badbf75021977a3dfae159a023b08
Nitsan Avni <nitsanav@gmail.com>
2023-07-19 12:43:31 +0000

    rm fizzbuzz

--- fizzbuzz.py
+++
@@ -1,2 +0,0 @@
-def fizzbuzz(number: int):
-    return 'Fizz' if number % 3 == 0 else number
--- test_fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from fizzbuzz import fizzbuzz
-
-class TestFizzBuzz(unittest.TestCase):
-    def test_fizzbuzz(self):
-        self.assertEqual(fizzbuzz(3), 'Fizz')
-
-if __name__ == '__main__':
-    unittest.main()
