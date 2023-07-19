b3d42b8aec72ca7bbf2b0ed47afdbcebf2775a5f
Nitsan Avni <nitsanav@gmail.com>
2023-07-19 16:39:29 +0000

    Implemented and tested the requirement a cell with more than three neighbours dies from overpopulation

--- gol.py
+++ gol.py
@@ -2,3 +2,5 @@ def cell_dies(number_of_neighbours: int) -> bool:
     return number_of_neighbours < 2
 def lives_on(num_neighbours: int) -> bool:
     return num_neighbours in [2, 3]
+def dies_from_overpopulation(number_of_neighbours: int) -> bool:
+    return number_of_neighbours > 3
--- test_gol.py
+++ test_gol.py
@@ -1,11 +1,9 @@
 import unittest
-from gol import cell_dies, lives_on
-
+from gol import cell_dies, lives_on, dies_from_overpopulation
 class TestGameOfLife(unittest.TestCase):
-
     def test_cell_with_less_than_two_neighbours_dies(self):
         self.assertTrue(cell_dies(1))
-
     def test_cell_with_two_or_three_neighbours_lives_on(self):
         self.assertTrue(lives_on(2))
-
+    def test_cell_with_more_than_three_neighbours_dies(self):
+        self.assertTrue(dies_from_overpopulation(4))
