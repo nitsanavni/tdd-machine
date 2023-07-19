af26117d60d62764b6e9c8f2b2318af9893a7260
Nitsan Avni <nitsanav@gmail.com>
2023-07-19 15:55:47 +0000

    Add initial test for fizzbuzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,7 +1,7 @@
 import unittest
-from fizzbuzz import fizzbuzz
-class FizzBuzzTest(unittest.TestCase):
-    def test_fizzbuzz_of_1_is_1(self):
-        self.assertEqual(fizzbuzz(1), 1)
-if __name__ == '__main__':
-    unittest.main()
+
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
