ad2f62279ac73c98b79a1c59518273060db75a1c
Nitsan Avni <nitsanav@gmail.com>
2023-07-20 08:07:20 +0000

    Implemented and refactored the fizzbuzz function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,2 @@
-def fizzbuzz(number: int):
-    if number % 3 == 0 and number % 5 == 0: return 'FizzBuzz'
-    elif number % 3 == 0: return 'Fizz'
-    elif number % 5 == 0: return 'Buzz'
-    else: return number
+def fizzbuzz(number: int, check_list=[(3, 'Fizz'), (5, 'Buzz')]):
+    return ''.join((str(string) if number % divisor == 0 else '') for divisor, string in check_list) or number
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,10 +1,13 @@
 import unittest
 from fizzbuzz import fizzbuzz
 class TestFizzBuzz(unittest.TestCase):
-    def test_fizzbuzz(self):
-        self.assertEqual(fizzbuzz(3), 'Fizz')
-        self.assertEqual(fizzbuzz(5), 'Buzz')
-        self.assertEqual(fizzbuzz(15), 'FizzBuzz')
+    def test_number(self):
         self.assertEqual(fizzbuzz(2), 2)
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz(3), 'Fizz')
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz(5), 'Buzz')
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(15), 'FizzBuzz')
 if __name__ == '__main__':
     unittest.main()
